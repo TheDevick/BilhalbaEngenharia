e261371cf768b66b5f3e8a556de3a8610722f1a2
TheDevick <erick@bilhalba.com.br>
2024-05-22 17:03:13 -0300

    fix: header fade in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
+import Content from "@/app/components/content/content";
 import Header from "@/app/components/header";
 import Intro from "@/app/components/intro";
 import Nav from "@/app/components/nav/nav";
 
 export default function Home() {
 	return (
-		<div className="is-preload">
-			<div id="wrapper" className="fade-in">
-				<Intro />
-				<Header />
-				<Nav />
-			</div>
+		<div id="wrapper" className="fade-in">
+			<Intro />
+			<Header />
+			<Nav />
+			<Content />
 		</div>
 	);
 }

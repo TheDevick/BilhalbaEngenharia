bf23eba11f5eea7232a50f99314eb039665b25cc
TheDevick <erick@bilhalba.com.br>
2024-05-22 16:48:20 -0300

    feat: create Intro Componentn

---
+++ src/app/components/intro.tsx
@@ -0,0 +1,17 @@
+export default function Intro() {
+  return (
+    <div id="intro">
+      <h1>
+        Bilhalba Engenharia: <br /> Projetos Elétricos
+      </h1>
+      <p>
+        Oferecendo serviços de engenharia elétrica e energia solar às cidades da fronteira de Quaraí, Artigas e região
+      </p>
+      <ul className="actions">
+        <li>
+          <a href="#header" className="button icon solid solo fa-arrow-down scrolly">Continue</a>
+        </li>
+      </ul>
+    </div>
+  );
+}

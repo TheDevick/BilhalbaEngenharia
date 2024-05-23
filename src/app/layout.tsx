f691c570f7e718aaf042d084a58344c6c7424f5d
TheDevick <erick@bilhalba.com.br>
2024-05-23 00:58:04 -0300

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
+import Copyright from "@/app/components/content/copyright";
 import ContentFooter from "@/app/components/content/footer";
+import Header from "@/app/components/content/header";
+import Intro from "@/app/components/content/intro";
+import Nav from "@/app/components/content/nav/nav";
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import Script from "next/script";
 import "../../public/assets/css/fontawesome-all.min.css";
 import "../../public/assets/css/main.css";
 import "../../public/assets/css/no-script.css";
-import Header from "@/app/components/content/header";
-import Intro from "@/app/components/content/intro";
-import Nav from "@/app/components/content/nav/nav";
-import Copyright from "@/app/components/content/copyright";
 
 export const metadata: Metadata = {
   title: "Bilhalba Engenharia: Projetos Elétricos",
@@ -32,6 +33,7 @@ export default function RootLayout({
           <ContentFooter />
           <Copyright />
         </div>
+        <Analytics />
         <Script src="/assets/js/jquery.min.js" />
         <Script src="/assets/js/jquery.scrollex.min.js" />
         <Script src="/assets/js/jquery.scrolly.min.js" />

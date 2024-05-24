ba35b3095eb70747fb825e71a99e8d729db2680b
TheDevick <erick@bilhalba.com.br>
2024-05-23 22:37:44 -0300

    feat: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,36 @@
-import { MetadataRoute } from 'next'
- 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+import { allPosts, Post as PostType } from 'contentlayer/generated';
+import { MetadataRoute } from 'next';
+
+const domain = 'https://bilhalba.com.br'
+
+function buildSitemapForPost(post: PostType) {
+  
+}
+
+export default function sitemap() {
+  const sitemap = [
     {
-      url: 'https://bilhalba.com.br',
+      url: domain,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://bilhalba.com.br/sobre-nos',
+      url: domain + '/sobre-nos',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.8,
-    },
+    }
   ]
+
+  allPosts.forEach((post: PostType) => {
+    sitemap.push({
+      url: domain + post.slug,
+      lastModified: new Date(post.date),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    })
+  })
+
+  return sitemap
 }

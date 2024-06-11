a8cb23e10478c3526d9b211913d3d6bf4d031983
TheDevick <erick@bilhalba.com.br>
2024-06-11 19:59:22 -0300

    build: install sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import { withContentlayer } from 'next-contentlayer2'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
+const nextConfig = {
+  sassOptions: {
+    includePaths: [
+      path.join(path.dirname(fileURLToPath(import.meta.url)), 'styles'),
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 export default withContentlayer(nextConfig)

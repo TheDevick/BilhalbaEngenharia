b47d4e020468b222b820b3208816f991fced54f9
TheDevick <erick@bilhalba.com.br>
2025-05-30 15:53:26 -0300

    changes

--- src/components/Home/Featured.tsx
+++ src/components/Home/Featured.tsx
@@ -6,15 +6,15 @@ export default function Featured() {
   return (
     <article className="post featured">
       <MajorHeading.Root>
-        <h2>
-          Energia Solar: A solução para um futuro mais sustentável e econômico
-        </h2>
         <Image
           src={Banner}
           height={500}
           alt="Abella Bilhalba Engenharia"
           style={{ height: 300, width: '100%', objectFit: 'contain' }}
         />
+        <h2>
+          Energia Solar: A solução para um futuro mais sustentável e econômico
+        </h2>
         <MajorHeading.Description>
           O futuro já chegou! Invista em Energia Solar e tenha uma grande
           redução na sua conta de luz.
